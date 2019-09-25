c4e71003ec553fae2f3408aa0cc3c9c572ebd545
Luís Fernando Moraes <luisfernando.pereira.moraes@gmail.com>
2019-09-24 21:10:03 -0300

    refactor

--- MovideskCodeChallenge.RiverCrossing/Program.cs
+++ MovideskCodeChallenge.RiverCrossing/Program.cs
@@ -12,7 +12,7 @@ namespace MovideskCodeChallenge.RiverCrossing
             var goat = new Character("Cabra");
             var hay = new Character("Feno");
 
-            
+
             goat.IsDevouredBy = lion;
             hay.IsDevouredBy = goat;
 
@@ -20,41 +20,48 @@ namespace MovideskCodeChallenge.RiverCrossing
             var leftRiverBank = new RiverBank("Esquerda");
             var personagens = new List<Character>
             {
-                lion,
                 goat,
-                hay
+                hay,
+                lion
             };
             leftRiverBank.Items = personagens;
 
             River = new River(rightRiverBank, leftRiverBank);
 
-            while (River.RightRiverBank.Items.Count < 3)
+            // Enquanto os 3 não tiverem cruzado o rio para o lado direito
+            while (River.LeftRiverBank.Items.Count != 0)
             {
-                Character pickedCharacter = null;
 
-                while (pickedCharacter == null && (River.CurrentRiverBank.Items.Count > 1 || River.CurrentRiverBank == River.LeftRiverBank))
+                Character pickedCharacter;
+
+                do
                 {
                     pickedCharacter = River.CurrentRiverBank.Items[new Random().Next(0, River.CurrentRiverBank.Items.Count - 1)];
 
                     var remaining = River.CurrentRiverBank.Items.FindAll(p => p != pickedCharacter);
 
-                    if (remaining.Count <= 1)
+                    if (remaining.Count == 2)
                     {
-                        if (remaining.Count <= 0) continue;
+                        if (remaining[0].IsDevouredBy == remaining[1] || remaining[1].IsDevouredBy == remaining[0])
+                        {
+                            pickedCharacter = null;
+                        }
+                    }
 
-                        if (remaining[0].IsDevouredBy == pickedCharacter ||
-                            pickedCharacter.IsDevouredBy == remaining[0] ||
-                            River.CurrentRiverBank != River.RightRiverBank) continue;
-
+                    else if (remaining.Count == 0 && River.CurrentRiverBank == rightRiverBank)
+                    {
                         pickedCharacter = null;
                         break;
                     }
 
-                    if (remaining[0].IsDevouredBy == remaining[1] || remaining[1].IsDevouredBy == remaining[0])
+                    else if(remaining.Count == 1 && pickedCharacter.IsDevouredBy != remaining[0] && remaining[0].IsDevouredBy != pickedCharacter && River.CurrentRiverBank != leftRiverBank )
                     {
                         pickedCharacter = null;
+                        break;
                     }
-                }
+
+                } while (pickedCharacter == null);
+
 
                 PlayMove(pickedCharacter);
             }
